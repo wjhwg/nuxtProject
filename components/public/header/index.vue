0bb2ed6d2493d2b01eb920cd8ba3a961eba5483a
1466619197@qq.com <1466619197@qq.com>
2019-07-06 17:08:34 +0800

    header

---
+++ components/public/header/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="m-header">
+    <el-row>
+      <top-bar />
+    </el-row>
+  </div>
+</template>
+<script>
+import topBar from './topbar.vue'
+export default {
+  components: {
+    topBar
+  }
+}
+</script>
+<style lang="scss">
+  @import "@/assets/css/public/layout.scss"
+</style>
